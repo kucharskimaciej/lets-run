b62437bda28979123c4fb24d26408c4277ad09db
Maciej Kucharski <maciek.zir@gmail.com>
2016-06-04 23:54:43 +0200

    [master][feature] session based auth

--- index.js
+++ index.js
@@ -4,12 +4,36 @@ const serve = require('koa-static');
 const send = require('koa-send');
 const route = require('koa-route');
 
+const session = require('koa-generic-session');
+const redisSession = require('koa-redis')({
+    url: '//localhost:6379'
+});
+const uuid = require('node-uuid').v4;
+
 const {join} = require('path');
 
 const app = koa();
 
+// ASSETS
 app.use(serve(join(__dirname, 'public')));
+
+// SESSION
+const YEAR_IN_MS = 31536000000;
+app.keys = ['devkey'];
+app.use(session({
+    store: redisSession,
+    cookie: {
+        maxAge: YEAR_IN_MS
+    }
+}));
+
+
+// APPLICATION ROUTES
 app.use(route.get('/*', function* () {
+    if (!this.session.token) {
+        this.session.token = uuid();
+    }
+
     yield send(this, 'index.html', { root: __dirname + '/views' });
 }));
 
